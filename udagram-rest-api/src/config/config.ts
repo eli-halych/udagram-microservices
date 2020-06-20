750f05079294ede9d74eb5478f26f788b121a0ed
Illia Halych <eli.halych@gmail.com>
2020-06-20 21:25:29 +0200

    FIX: restore REST API

---
+++ udagram-rest-api/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESQL_USAERNAME,
+    "password": process.env.POSTGRESQL_PASSWORD,
+    "database": process.env.POSTGRESQL_DATABASE,
+    "host": process.env.POSTGRESQL_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUSKET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+}

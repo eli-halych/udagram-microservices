bc5e4feecf52ad1d0fac38c116c03ac86c73bee6
Illia Halych <eli.halych@gmail.com>
2020-06-20 18:35:42 +0200

    ADD: generate salt in 10 rounds

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -11,7 +11,8 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const saltRounds = 10;
+    return await bcrypt.genSalt(saltRounds);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {

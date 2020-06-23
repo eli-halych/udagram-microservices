26b4cee2476b910b16420589cff97582706b3598
Illia Halych <eli.halych@gmail.com>
2020-06-23 12:25:31 +0200

    [UPD]: update frontend's Dockerfile
    - expose 8100

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-user-dev.cnkn1nepzjym.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramuserdev
+export AWS_BUCKET=udagramdev-0001
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+#export PORT=8080
+export JWT_SECRET=hellosecret
 export URL=http://localhost:8100
